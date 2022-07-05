db61a62b95023db09f761c23e7cf17eed066157b
Carlos L Ramirez <carlos.ramirez@syprotec.com.gt>
2022-07-04 18:20:06 -0600

    This is my first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Hola mundo
+
+Aprendiendo GIT en a cloud guru
+
+## 2022 Goal
+
+- AWS Solution Architect Professional
+- Aprender DevOps
+- Documentar labs y proyectos
---
+++ src/hi-there.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// hi-there.cpp
+// A simple aplication
+
+using namespace std;
+
+int main()
+{
+  count << "Hi there! Welcome to Linux Academy (now a cloud guru)" << endl;
+  return 0;
+}
